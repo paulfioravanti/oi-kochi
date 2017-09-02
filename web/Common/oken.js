7796082b9f5905b05e3d1331150af936fd21e910
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-09-02 21:00:41 +1000

    Initial commit

---
+++ web/Common/oken.js
@@ -0,0 +1,2 @@
+nihongobutton=new Image(90,30); nihongobutton2=new Image(90,30);
+nihongobutton.src="Common/nihongobutton.gif"; nihongobutton2.src="Common/nihongobutton2.gif";
---
+++ web/Common/okja.js
@@ -0,0 +1,2 @@
+englishbutton=new Image(105,30); englishbutton2=new Image(105,30);
+englishbutton.src="Common/englishbutton.gif"; englishbutton2.src="Common/englishbutton2.gif;
